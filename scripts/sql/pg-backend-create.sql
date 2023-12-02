8e0ba4955063758a1a8d711727e85d7ec4fc1880
quietinvestor <4750936+quietinvestor@users.noreply.github.com>
2023-12-02 14:39:54 +0100

    Fix SQL script permissions

--- scripts/sql/pg-backend-create.sql
+++ scripts/sql/pg-backend-create.sql
@@ -1,13 +1,16 @@
 CREATE SCHEMA IF NOT EXISTS :schema_name;
 CREATE SEQUENCE IF NOT EXISTS :schema_name.:sequence_name AS bigint;
-CREATE TABLE IF NOT EXISTS :schema_name.:table_name (
+CREATE TABLE IF NOT EXISTS :schema_name.states (
     id bigint NOT NULL DEFAULT nextval(:'full_sequence_name') PRIMARY KEY,
     name text UNIQUE,
     data text
 );
+CREATE UNIQUE INDEX IF NOT EXISTS states_by_name ON :schema_name.states (name);
 CREATE role :group_name;
+GRANT USAGE ON SCHEMA :schema_name TO :group_name;
+GRANT ALL ON SEQUENCE :schema_name.:sequence_name TO :group_name;
 GRANT ALL
-ON :schema_name.:table_name
+ON :schema_name.states
 TO :group_name;
 CREATE role :user_name
 LOGIN
