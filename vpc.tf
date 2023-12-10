f234cd0f05a807e0f714787a30e07c162db2fa65
quietinvestor <4750936+quietinvestor@users.noreply.github.com>
2023-12-10 14:00:44 +0100

    Append environment to VPC name

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 resource "digitalocean_vpc" "postgresql" {
   description = var.do_vpc_postgresql_description
   ip_range    = var.do_vpc_postgresql_ip_range
-  name        = "vpc-postgresql-${var.do_region}-${var.do_project_name}"
+  name        = "vpc-postgresql-${var.do_region}-${var.do_project_name}-${var.do_environment}"
   region      = var.do_region
 }
